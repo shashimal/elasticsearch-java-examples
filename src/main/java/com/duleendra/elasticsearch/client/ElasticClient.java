b5a77894d12fd4db01dd659848c0377b6829fca4
shashimal <shashimald@gmail.com>
2017-04-05 23:02:03 +0800

    Implementation for the client class

--- src/main/java/com/duleendra/elasticsearch/client/ElasticClient.java
+++ src/main/java/com/duleendra/elasticsearch/client/ElasticClient.java
@@ -1,7 +1,29 @@
 package com.duleendra.elasticsearch.client;
 
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * Created by Duleendra on 5/4/17.
  */
 public class ElasticClient {
+
+    private final static String ES_HOSTS[] = {"localhost"};
+    private final static int port = 9300;
+
+    private static Client client;
+
+    public static Client getClient() {
+        try {
+            client = TransportClient.builder().build()
+                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return client;
+    }
 }
